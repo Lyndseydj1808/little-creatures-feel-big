24275a5696f508150f96b27470e485a9742380c3
Lyndseydj1808 <lyndseydj1808@gmail.com>
2026-09-14 21:53:46 -0500

    updated css for parent-dashboard

--- react-front-end-app/src/childPages/ChooseAnAccount.jsx
+++ react-front-end-app/src/childPages/ChooseAnAccount.jsx
@@ -1,13 +1,52 @@
-import React, { useCallback, useState, useEffect } from "react";
-import { API_URL } from "../services/apiConfig";
+import React from "react";
+import { Link } from "react-router-dom";
 import LoadingSpinner from "../components/LoadingSpinner";
 import HomeButton from "../components/HomeButton";
 import ChildAccounts from "../parentPages/ChildAccounts";
+import useChildAccounts from "../hooks/useChildAccounts";
 
 export default function ChooseAnAccount() {
+    const { childAccounts, loading, loadError } = useChildAccounts();
+
+
+if (!loadError) {
 return (
 <main className="choose-account-contianer">
-  <ChildAccounts />
-</main>
-);
+  <div className="parent-logged-in">
+  {loading && (
+    <div className="loading-placeholder">
+      <LoadingSpinner />
+    </div>
+  )}
+  {loadError && <p>Error: {loadError}</p>}
+  {childAccounts.map((child) => (
+    <Link
+    className="button-to-main-games"
+    to={`/mini-games/${child.childId}`}
+    key={child.childId}
+    >
+      <p>{child.name}</p>
+    </Link> 
+  ))}
+  <p>OR</p>
+  <Link 
+  className="guest"
+  to="/mini-games">
+    <p>Play Now as a guest!</p>
+    </Link>
+    </div>
+    </main>
+    )}
+
+  return (
+    <div className="no-parent-account">
+      <p>Please have a parent log in to play with your account.</p>
+      <p>OR</p>
+        <Link 
+  className="guest"
+  to="/mini-games">
+    <p>Play Now as a guest!</p>
+    </Link>
+    </div>
+  );
 }
--- react-front-end-app/src/parentPages/ParentDashboard.jsx
+++ react-front-end-app/src/parentPages/ParentDashboard.jsx
@@ -8,7 +8,6 @@ export default function ParentDashboard() {
       <div className="parent-nav-buttons">
         <Link className="create-child-account-button" to="/create-child-account">Create Child Account</Link>
         <Link className="view-child-account-button" to="/child-accounts">View Child Accounts</Link>
-        <Link className="set-time-limit-button" to="/time-limit">Set Time Limit</Link>
       </div>
     </main>
   )
