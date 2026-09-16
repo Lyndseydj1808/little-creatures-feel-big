4b16ba713679ea729961359a41a46c243eba9065
Lyndseydj1808 <lyndseydj1808@gmail.com>
2026-09-16 11:30:22 -0500

    updated css for choose-an-account page

--- react-front-end-app/src/childPages/ChooseAnAccount.jsx
+++ react-front-end-app/src/childPages/ChooseAnAccount.jsx
@@ -1,52 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../components/LoadingSpinner";
-import HomeButton from "../components/HomeButton";
-import ChildAccounts from "../parentPages/ChildAccounts";
 import useChildAccounts from "../hooks/useChildAccounts";
+import "../childPages/ChooseAnAccount.css";
 
 export default function ChooseAnAccount() {
-    const { childAccounts, loading, loadError } = useChildAccounts();
+  const { childAccounts, loading, loadError } = useChildAccounts();
 
-
-if (!loadError) {
-return (
-<main className="choose-account-contianer">
-  <div className="parent-logged-in">
-  {loading && (
-    <div className="loading-placeholder">
-      <LoadingSpinner />
-    </div>
-  )}
-  {loadError && <p>Error: {loadError}</p>}
-  {childAccounts.map((child) => (
-    <Link
-    className="button-to-main-games"
-    to={`/mini-games/${child.childId}`}
-    key={child.childId}
-    >
-      <p>{child.name}</p>
-    </Link> 
-  ))}
-  <p>OR</p>
-  <Link 
-  className="guest"
-  to="/mini-games">
-    <p>Play Now as a guest!</p>
-    </Link>
-    </div>
-    </main>
-    )}
+  if (!loadError) {
+    return (
+      <main className="choose-account-contianer">
+        <div className="parent-logged-in">
+          {loading && (
+            <div className="loading-placeholder">
+              <LoadingSpinner />
+            </div>
+          )}
+          {loadError && <p>Error: {loadError}</p>}
+          {childAccounts.map((child) => (
+            <Link
+              className="child-button-to-main-games"
+              to={`/mini-games/${child.childId}`}
+              key={child.childId}
+            >
+               {child.name}
+            </Link>
+          ))}
+          <span className="or-divider">OR</span>
+          <Link className="guest" to="/mini-games">
+            ▶️ Play Now as a guest!
+          </Link>
+        </div>
+      </main>
+    );
+  }
 
   return (
     <div className="no-parent-account">
-      <p>Please have a parent log in to play with your account.</p>
-      <p>OR</p>
-        <Link 
-  className="guest"
-  to="/mini-games">
-    <p>Play Now as a guest!</p>
-    </Link>
+      <p className="no-parent">
+        Please have a parent log in to play with your account.
+      </p>
+      <span className="or-divider">OR</span>
+      <Link className="guest" to="/mini-games">
+        ▶️ Play Now as a guest!
+      </Link>
     </div>
   );
 }
